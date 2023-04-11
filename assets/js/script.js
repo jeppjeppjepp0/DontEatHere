2792789676b066d69a8adeb4f0124f65df3b87a4
Kaiden Parcher <ktparcher@.com>
2023-04-11 11:05:22 -0700

    inor debugging changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ var formSubmitHandler = function (event) {
           alert('Please enter proper city name');
         }
     };
-    
+
     var getCityLocation = function(city){
     var cityUrl = 'https://api.openweathermap.org/geo/1.0/direct?q='+ city + '&appid=e7061ed9c868477223ac6802888315d2';
 
@@ -24,7 +24,7 @@ var formSubmitHandler = function (event) {
       if (response.ok) {
         response.json().then(function (data) {
           console.log(data)
-          getWeather(data, varLat, varLon);
+        //   getWeather(data, varLat, varLon);
           varLat = data[0].lat;
           varLon = data[0].lon;
           console.log(varLat)

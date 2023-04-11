8b867cf7baa8a9925d1192a183469b1c823653e9
Scarlett Borroel <scarlett@Scarletts-MacBook-Pro.local>
2023-04-11 14:17:01 -0700

    addedjs

--- assets/js/result.js
+++ assets/js/result.js
@@ -1,3 +1,31 @@
+var yelpEndpoint = "https://api.yelp.com/v3/business/search";
+
+var params = {
+    Term: "restaurants",
+    location: "San Francisco",
+    limit: 3
+};
+
+var headers = { 
+    Authorization:"Bearer 70Qz_TnQu1CogX0wCS1CoMbduwaXZhLz_Mk2UY8nPdkSjVqwUTphv4secp5O5YT83wQt-DvkZBPAGDfOU25DNxeCrDnlY2kB5MF0tSFbFqMQS-Xy_lflAezsmnQ0ZHYx"
+};
+
+fetch(`${yelpEndpoint}?${new URLSearchParams(params)}`, {
+
+})
+
+.then(response => response.json())
+.then(data => {
+
+    data.business.forEach(business => {
+        console.log(business.name);
+    });
+
+})
+.catch(error => {
+    console.error(error);
+});
+
 var returnHomeBtn = document.querySelector("#return-home");
 
 function handleReturnHome(){
@@ -5,6 +33,8 @@ function handleReturnHome(){
 }
 
 returnHomeBtn.addEventListener("click", handleReturnHome)
+
+
 
 /*
 read city name

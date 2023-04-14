0e38df4bae6e492e24b608ece17a69f5d08fdc0b
jeppjeppjepp0 <jeddmj1@gmail.com>
2023-04-14 01:54:08 -0600

    searching function fixed /s

---
+++ assets/js/result.js
@@ -0,0 +1,79 @@
+var latitude = localStorage.getItem("lat");
+var longitude = localStorage.getItem("lon");
+
+var sunriseElement = document.querySelector('#sunrise-time');
+var sunsetElement = document.querySelector('#sunset-time');
+var cityElement = document.querySelector('#searched-city');
+
+var sunInfo1;
+var sunInfo2;
+
+function displayCityName() {
+    if(localStorage.getItem('cityName')){
+        cityElement.textContent = localStorage.getItem('cityName');
+    }else {
+        cityElement.textContent = "UNKNOWN";
+    }
+};
+
+function displaySunriseSunset() {
+    console.log(sunInfo1);
+    console.log(sunInfo2);
+    document.querySelector("#sunrise-1").textContent = sunInfo1.sunrise;
+    document.querySelector("#sunrise-2").textContent = sunInfo2.sunrise;
+};
+
+function searchSunriseSunsetToday(lat, lon){
+    fetch('https://api.sunrise-sunset.org/json?lat=' + lat + '&lng=' + lon + '&date=today')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            localStorage.setItem('sunInfo1', JSON.stringify(data.results));
+        })
+        .catch(error => {
+            console.error(error);
+            return null;
+        });
+}
+
+function searchSunriseSunsetEquinox(lat, lon){
+    fetch('https://api.sunrise-sunset.org/json?lat=' + lat + '&lng=' + lon + '&date=2023-07-21')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            localStorage.setItem('sunInfo2', JSON.stringify(data.results));
+        })
+        .catch(error => {
+            console.error(error);
+            return null;
+        });
+}
+
+displayCityName();
+
+searchSunriseSunsetToday(latitude, longitude);
+sunInfo1 = JSON.parse(localStorage.getItem('sunInfo1'));
+
+searchSunriseSunsetEquinox(latitude, longitude, "2023-07-21");
+sunInfo2 = JSON.parse(localStorage.getItem('sunInfo2'));
+
+displaySunriseSunset();
+
+
+
+var returnHomeBtn = document.querySelector("#return-home");
+
+function handleReturnHome(){
+    document.location = "./index.html";
+}
+
+returnHomeBtn.addEventListener("click", handleReturnHome)
+
+
+
+/*
+read city name
+print 3 results from yelp
+event reader to return to home screen
+    on click, return to home screen
+*/
